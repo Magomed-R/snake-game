daf848726dafc807c352f30e2d5458117a1d7181
Magomed-R <magomed-radzhabov@internet.ru>
2023-12-10 14:18:35 +0400

    base game engine

---
+++ README.md
@@ -0,0 +1,11 @@
+# Snake game
+
+![](./assets/Zmeika.gif)
+## Description
+Snake is my game created in native javascript. 
+## Get started
+To start playing you can follow the link below:
+https://github.com<br/>
+or<br/>
+clone the repository and run the index.html on your browser.<br/>
+Enjoy it!
---
+++ js/index.js
@@ -0,0 +1,121 @@
+const fieldsContainer = document.querySelector(".fields__container");
+
+let canChangeVector = true;
+let vector = "ArrowRight";
+let fields = [...Array(15)].map((el, i) => (el = [...Array(17)].map((el, j) => (el = 0))));
+let tail = {
+    x: 0,
+    y: 0,
+};
+let head = {
+    x: 1,
+    y: 0,
+};
+const history = [{ ...tail }, { ...head }];
+fields[tail.y][tail.x] = 1;
+fields[head.y][head.x] = 1;
+const vectorSides = ["ArrowUp", "ArrowRight", "ArrowDown", "ArrowLeft"]
+
+createApple(fields);
+
+for (let i = 0; i < fields.length; i++) {
+    fieldsContainer.innerHTML += "<div class='fields__row'></div>";
+}
+
+const fieldsRow = document.querySelectorAll(".fields__row");
+
+render(fields, fieldsRow);
+
+let interval = setInterval(function run() {
+    fields[history[0].y][history[0].x] = 0;
+    
+    switch (vector) {
+        case "ArrowUp":
+            if (head?.y > 0 && fields[head.y - 1][head.x] !== 1) {
+                if (fields[head.y - 1][head.x] === 2) {
+                    createApple(fields);
+                } else history.shift()
+                fields[--head.y][head.x] = 1;
+            } else {
+                clearInterval(interval);
+            }
+            break;
+        case "ArrowRight":
+            if (head.x < 16 && fields[head.y][head.x + 1] !== 1) {
+                if (fields[head.y][head.x + 1] === 2) {
+                    createApple(fields);
+                } else history.shift()
+                fields[head.y][++head.x] = 1;
+            } else {
+                clearInterval(interval);
+            }
+            break;
+        case "ArrowDown":
+            if (head.y < 14 && fields[head.y + 1][head.x] !== 1) {
+                if (fields[head.y + 1][head.x] === 2) {
+                    createApple(fields);
+                } else history.shift()
+                fields[++head.y][head.x] = 1;
+            } else {
+                clearInterval(interval);
+            }
+            break;
+        case "ArrowLeft":
+            if (head.x > 0 && fields[head.y][head.x - 1] !== 1) {
+                if (fields[head.y][head.x - 1] === 2) {
+                    createApple(fields);
+                } else history.shift()
+                fields[head.y][--head.x] = 1;
+            } else {
+                clearInterval(interval);
+            }
+            break;
+    }
+
+    history.push({ x: head.x, y: head.y });
+
+    canChangeVector = true;
+
+    render(fields, fieldsRow);
+}, 200);
+
+document.addEventListener("keydown", function (event) {
+    if (
+        canChangeVector &&
+        !(vector == "ArrowUp" && event.key == "ArrowDown") &&
+        !(vector == "ArrowDown" && event.key == "ArrowUp") &&
+        !(vector == "ArrowRight" && event.key == "ArrowLeft") &&
+        !(vector == "ArrowLeft" && event.key == "ArrowRight") &&
+        (vectorSides.includes(event.key))
+    ) {
+        vector = event.key;
+        canChangeVector = false;
+    }
+});
+
+function render(fieldsNode, fieldsRowNode) {
+    for (let i = 0; i < fieldsNode.length; i++) {
+        fieldsRowNode[i].innerHTML = "";
+        for (let j = 0; j < fieldsNode[i].length; j++) {
+            let classes = "field";
+            let field = fieldsNode[i][j];
+
+            if (field === 0) classes += " empty";
+            else if (field === 1) classes += " zmeika";
+            else if (field === 2) classes += " apple";
+
+            fieldsRowNode[i].innerHTML += `<div class='${classes}'></div>`;
+        }
+    }
+}
+
+function createApple(fieldsNode) {
+    let x = randomInteger(0, 14);
+    let y = randomInteger(0, 16);
+    if (fieldsNode[x][y] === 0) fieldsNode[x][y] = 2;
+}
+
+function randomInteger(min, max) {
+    let rand = min + Math.random() * (max + 1 - min);
+    return Math.floor(rand);
+}
